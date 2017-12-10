6caa1bd629aa1a80da6d9f6afe1c42066408828b
Monsij Biswal <biswalmonsij@gmail.com>
2017-12-10 12:42:39 +0530

    Add starbox builder

---
+++ starbox.py
@@ -0,0 +1,23 @@
+#monsijb
+def starbox(width, height):
+    """print a box made up of asterisks.
+
+    width: width of box in characters, must be at least 2
+    height: height of box in lines, must be at least 2
+    """
+    print("*" * width) #print top edge of box
+   
+    # print sides of box
+    # todo: print this line height-2 times, instead of three times
+    for i in range(height-2):
+        print("*" + " " * (width-2) + "*") 
+    
+
+    print("*" * width) #print bottom edge of box
+
+# Test Cases
+print("Test 1:")
+starbox(5, 5) # this prints correctly
+
+print("Test 2:")
+starbox(3, 4)  # this currently prints two lines too tall - fix it!
